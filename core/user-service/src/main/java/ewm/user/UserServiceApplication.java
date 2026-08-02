1a167a82714d074c9e130a9fee06c182bb34c661
Voloskov Ivan <voloskov.ivan03@mail.ru>
2026-08-02 13:59:19 +0300

    Решена последняя ошибка с добавлением пользователя с сущ. email

--- core/user-service/src/main/java/ewm/user/UserServiceApplication.java
+++ core/user-service/src/main/java/ewm/user/UserServiceApplication.java
@@ -3,9 +3,11 @@ package ewm.user;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@ComponentScan(basePackages = "ewm")
 public class UserServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserServiceApplication.class, args);
